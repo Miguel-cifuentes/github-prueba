67619fcc7fbe41e4f3d7a9dabb98517d23eddf1c
Miguel Cifuentes <miguelcifuentes@ustavillavo.edu.co>
2026-04-10 09:10:28 -0500

    Add: agregue un nuevo endpoint

--- src/main/java/com/example/githubrepaso/controller/helloWordController.java
+++ src/main/java/com/example/githubrepaso/controller/helloWordController.java
@@ -1,4 +1,22 @@
 package com.example.githubrepaso.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/HelloWord")
 public class helloWordController {
+
+    @RequestMapping("/world")
+    public String HelloWorld(){
+        return "Hello Word";
+    }
+
+    @RequestMapping ("/HelloMiguel")
+    public String HelloMiguel(){
+        return "Hello Miguel";
+    }
+
+
 }
