306fce59f2836782b140c5d25e61095d89c0f1a5
Miguel Cifuentes <miguelcifuentes@ustavillavo.edu.co>
2026-04-10 08:40:40 -0500

    First Commit: Created Proyect

---
+++ src/main/java/com/example/githubrepaso/GithubRepasoApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubrepaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepasoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepasoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubrepaso/GithubRepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubrepaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepasoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
